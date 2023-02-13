1cfde5f0f1ea5f089934a7008ef23068e07c3e61
Daria <ruksova17@gmail.com>
2023-02-13 22:12:09 +0400

    Example008_TaskWithDogAndFriends

---
+++ Lecture 1/Example008_TaskWithDogAndFriends/Program.cs
@@ -0,0 +1,26 @@
+﻿int distance = 20;
+int minDistance = 10;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int flag = 1;
+int time = 0;
+int count = 0;
+
+while (distance > minDistance)
+{
+    if (flag == 1)
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        flag = 2;
+    }
+    else
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        flag = 1;    
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count = count + 1;
+}
+
+Console.WriteLine("Собака пробежит от одного друга к другому " + count + " раз");
