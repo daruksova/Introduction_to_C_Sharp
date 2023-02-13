5c28e82f29aa781aebd34165a08823bae97dda21
Daria <ruksova17@gmail.com>
2023-02-13 23:01:03 +0400

    Example005_LastDigit

---
+++ Seminar 1/Example005_LastDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000) 
+{
+    System.Console.WriteLine($"Последняя цифра введенного числа {number % 10}");
+}
+else 
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число");
+}
