6e7e8f46c3acf3425f1c044137aca87e5cf7b49a
Daria <ruksova17@gmail.com>
2023-02-13 22:13:10 +0400

    Example001_Square

---
+++ Seminar 1/Example001_Square/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Квадрат числа " + number + " равен " + number * number);
