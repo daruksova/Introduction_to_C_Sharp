96b132a067a541911931b7ec9090a76b2a3f3a5c
Daria <ruksova17@gmail.com>
2023-02-13 22:25:40 +0400

    Example002_IsItASquare

---
+++ Seminar 1/Example002_IsItASquare/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+System.Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+    System.Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    System.Console.WriteLine("Первое число не является квадратом второго");
+}
