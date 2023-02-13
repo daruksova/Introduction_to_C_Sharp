219b6bd99c06f7d95c1600127ea1d99bb8204eca
Daria <ruksova17@gmail.com>
2023-02-13 23:34:37 +0400

    Task3_Parity

---
+++ Homework 1/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine("Число четное");
+}
+else
+{
+    System.Console.WriteLine("Число нечетное");
+}
