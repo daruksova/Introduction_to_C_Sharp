c317055aebae7318c79c3be0b6383991a8697b80
Daria <ruksova17@gmail.com>
2023-02-15 21:53:54 +0400

    Seminar2_Example01

---
+++ Seminar 2/Example01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Random random = new Random();
+int number = random.Next(10, 100);
+int figure1 = number / 10;
+int figure2 = number % 10;
+
+System.Console.WriteLine(number);
+
+if (figure1 >= figure2)
+{
+    System.Console.WriteLine(figure1);
+}
+else 
+{
+    System.Console.WriteLine(figure2);
+}
