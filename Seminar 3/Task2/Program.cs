a755888763ec065b45f1e0e4e6244b22765bd33a
Daria <ruksova17@gmail.com>
2023-02-19 14:47:44 +0400

    Seminar3_Task2

---
+++ Seminar 3/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (x и y)
+
+System.Console.WriteLine("Введите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (num == 1)
+{
+    System.Console.WriteLine("Диапазон возможных координат x > 0 и y > 0");
+}
+else if (num == 2)
+{
+    System.Console.WriteLine("Диапазон возможных координат x < 0 и y > 0");
+}
+else if (num == 3)
+{
+    System.Console.WriteLine("Диапазон возможных координат x < 0 и y < 0");
+}
+else if (num == 4)
+{
+    System.Console.WriteLine("Диапазон возможных координат x > 0 и y < 0");
+}
+else
+{
+    System.Console.WriteLine("Такой четверти не существует");
+} 
