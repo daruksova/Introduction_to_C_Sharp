5becdc6caa25a7647508ef45e657c918cd4ac4d7
Daria <ruksova17@gmail.com>
2023-02-24 14:28:43 +0400

    Homework4_Task1_DegreeOfNumber

---
+++ Homework 4/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int[] GetNumbers()
+{
+    int[] array = new int[2];
+    System.Console.Write("Введите число A: ");
+    array[0]= Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Введите число B: ");
+    array[1] = Convert.ToInt32(Console.ReadLine());
+    return array;
+}
+
+int DegreeOfNumber(int numA, int numB)
+{
+    int degree = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        degree *= numA;
+    }
+    return degree;
+}
+
+int[] array = GetNumbers();
+System.Console.WriteLine(DegreeOfNumber(array[0], array[1]));
