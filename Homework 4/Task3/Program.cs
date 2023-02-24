f2b3cb8f4cf2045270ef277bfe85b51f1732220f
Daria <ruksova17@gmail.com>
2023-02-24 15:30:24 +0400

    Homework4_Task3_GenerateArray

---
+++ Homework 4/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] CreateArray(int arrayLength)
+{
+    int[] array = new int[arrayLength];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0, 101);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine(String.Join(" ", array));
+};
+
+PrintArray(CreateArray(8));
