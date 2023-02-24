4a8a1613c4d13adc8a916d2ffc88cdc8a769038c
Daria <ruksova17@gmail.com>
2023-02-24 12:56:13 +0400

    Seminar4_Example03_ProductNumbers

---
+++ Seminar 4/Example03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисе от 1 до N
+
+int PrintNum(string text)
+{
+    System.Console.Write(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+int ProductNums(int N)
+{
+    int product = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        product *= i;
+    }
+    return product;
+}
+
+System.Console.WriteLine(ProductNums(PrintNum("Введите число: ")));
