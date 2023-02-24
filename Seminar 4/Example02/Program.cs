5b47c87004bec16edb21507320acb01729322051
Daria <ruksova17@gmail.com>
2023-02-24 12:24:25 +0400

    Seminar4_Example02_DigitCount

---
+++ Seminar 4/Example02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая приниимает на вход число и выдаёт количество цифр в числе
+
+int PrintNum(string text)
+{
+    System.Console.Write(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+int DigitCount(int number)
+{
+    int i = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        i++;
+    }
+    return i;
+}
+
+Console.WriteLine(DigitCount(PrintNum("Введете число: ")));
