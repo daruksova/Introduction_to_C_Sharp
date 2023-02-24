69a7c04d6a9ba31c6ad3c0eff23f3786de39c483
Daria <ruksova17@gmail.com>
2023-02-24 13:48:38 +0400

    Seminar4_Example04_PrintRandomArray

---
+++ Seminar 4/Example04/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+int[] CreateArray(int arrayLength)
+{
+    int[] array = new int[arrayLength];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine(String.Join(" ", array));
+};
+
+PrintArray(CreateArray(10));
