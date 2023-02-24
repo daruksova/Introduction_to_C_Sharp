b143efcd9402e61e46528abfab74d6ab822b6fe4
Daria <ruksova17@gmail.com>
2023-02-24 11:39:17 +0400

    Seminar4_Example01_Sum

---
+++ Seminar 4/Example01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число А и выдаёт сумму чисел от 1 до А.
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int sum = Sum(num);
+System.Console.WriteLine($"Сумма чисел от 1 до {num} равна {sum}");
