6a3a5ded5a576c03742533713bc1bba20d04cebc
Daria <ruksova17@gmail.com>
2023-02-27 21:29:53 +0400

    Lecture4_Example01_TwoDimensionalArray

---
+++ Lecture 4/Example01_TwoDimensionalArray/Program.cs
@@ -0,0 +1,25 @@
+﻿// Двумерный массив строк
+
+// string[,] table = new string[2, 5];
+// table[1, 2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         System.Console.WriteLine($"-{table[rows, columns]}- ");
+//     }
+// }
+
+// Двумерный массив чисел
+
+int[,] matrix = new int[3, 4];
+
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < matrix.GetLength(1); columns++)
+    {
+        System.Console.Write($"{matrix[rows, columns]} ");
+    }
+    System.Console.WriteLine();
+}
