081d3175d97f9a128be9aca815baba3873d3e0ae
Daria <ruksova17@gmail.com>
2023-03-01 22:01:09 +0400

    Seminar6_Example04_Fibonacci

---
+++ Seminar 6/Example04_Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа: 0 и 1.
+
+int GetNum()
+{
+    System.Console.Write("Введите число N: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[] Fibonacci(int number)
+{
+    int[] array = new int[number];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < number; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine(String.Join(", ", array));
+}
+
+int N = GetNum();
+PrintArray(Fibonacci(N));
