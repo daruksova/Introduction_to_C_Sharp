c6e04a82462258761ac8a51ca9188cbd2ab5aebc
Daria <ruksova17@gmail.com>
2023-03-01 22:20:58 +0400

    Seminar6_Example05_CopyArray

---
+++ Seminar 6/Example05_CopyArray/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] GenerateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + String.Join(", ", array) + "]");
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+
+int[] array = GenerateArray(9, -9, 9);
+PrintArray(array);
+int[] newArray = CopyArray(array);
+PrintArray(newArray);
