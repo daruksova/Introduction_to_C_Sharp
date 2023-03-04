49438efdce02934e889be809f400063ceabfec94
Daria <ruksova17@gmail.com>
2023-03-04 10:12:27 +0400

    Homework6_Task1_GreaterThanZero

---
+++ Homework 6/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] GetNumbers()
+{
+    System.Console.WriteLine("Введете числа через пробел:");
+    int[] array = Array.ConvertAll(Console.ReadLine()!.Split(" "), int.Parse);
+    return array;
+}
+
+int GreaterThanZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+int[] numbers = GetNumbers();
+System.Console.WriteLine("Чисел больше нуля введено " + GreaterThanZero(numbers));
