bd7de7469fea47c7ad1bf771d27a38154c7f261e
Daria <ruksova17@gmail.com>
2023-03-08 21:27:17 +0400

    Seminar7_Example03_optimization

--- Seminar 7/Example03/Program.cs
+++ Seminar 7/Example03/Program.cs
@@ -23,14 +23,11 @@ int[,] GenerateMatrix(int m, int n)
 
 void ChangeMatrix(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 1; i < matrix.GetLength(0); i += 2)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 1; j < matrix.GetLength(1); j += 2)
         {
-            if (i % 2 == 1 && j % 2 == 1)
-            {
-                matrix[i, j] = matrix[i, j] * matrix[i, j];
-            }
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
         }
     }
 }
