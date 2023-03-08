e370dde07cff2cd5e0f705b86156fb56c7dca8b5
Daria <ruksova17@gmail.com>
2023-03-08 20:58:19 +0400

    Seminar7_Example03

--- Seminar 7/Example03/Program.cs
+++ Seminar 7/Example03/Program.cs
@@ -1,6 +1,5 @@
-﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле:
-// a[m,n] = m+n
-// Выведите полученный массив на экран
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса
+// нечётные, и замените эти элементы на их квадраты.
 
 int ReadInt(string text)
 {
@@ -11,14 +10,29 @@ int ReadInt(string text)
 int[,] GenerateMatrix(int m, int n)
 {
     int[,] matrix = new int[m, n];
+    Random rand = new Random();
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            matrix[i, j] = i + j;
+            matrix[i, j] = rand.Next(0, 10);
         }
     }
     return matrix;
+}
+
+void ChangeMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 1 && j % 2 == 1)
+            {
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+            }
+        }
+    }
 }
 
 void PrintMatrix(int[,] matrix)
@@ -36,4 +50,7 @@ void PrintMatrix(int[,] matrix)
 int m = ReadInt("Введите количество строк матрицы: ");
 int n = ReadInt("Введите количество столбцов матрицы: ");
 var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+System.Console.WriteLine();
+ChangeMatrix(myMatrix);
 PrintMatrix(myMatrix);
