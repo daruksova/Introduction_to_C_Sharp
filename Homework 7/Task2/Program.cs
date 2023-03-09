0804e562f34ac1e84e4bc03bf3348a779e5e32af
Daria <ruksova17@gmail.com>
2023-03-09 21:52:31 +0400

    Homework07_Task2

---
+++ Homework 7/Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }    
+}
+
+void FindElement(int[,] matrix, int row, int col)
+{
+    if (row <= matrix.GetLength(0) && col <= matrix.GetLength(1))
+    {
+        System.Console.WriteLine(matrix[row - 1, col - 1]);
+    }
+    else
+    {
+        System.Console.WriteLine("Такого элемента в массиве нет.");
+    }
+}
+
+int m = ReadInt("Введите количество строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+int rowNumber = ReadInt("Введите номер строки элемента: ");
+int colNumber = ReadInt("Введите номер столбца элемента: ");
+FindElement(myMatrix, rowNumber, colNumber);
+
