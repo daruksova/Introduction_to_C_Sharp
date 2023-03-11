08f6d44c8c4226db327b23c96b50777ead50d1e8
Daria <ruksova17@gmail.com>
2023-03-11 21:02:02 +0400

    Homework8_Task1

---
+++ Homework 8/Task1/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] SortMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+			for (int l = 0; l < matrix.GetLength(1) - 1; l++)
+            {
+                if (matrix[i, l] < matrix[i, l + 1])
+                {
+                    int t = matrix[i, l + 1];
+                    matrix[i, l + 1] = matrix[i, l];
+                    matrix[i, l] = t;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+int rows = ReadInt("Введите количество строк массива: ");
+int cols = ReadInt("Введите количество столбцов массива: ");
+int[,] matrix = GenerateMatrix(rows, cols);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+PrintMatrix(SortMatrix(matrix));
